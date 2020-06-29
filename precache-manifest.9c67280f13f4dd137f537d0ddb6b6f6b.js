b4e68ef57b2bccb5635109a2d5d2869676c62859
AlexanderDergachev <31667079+AlexanderDergachev@users.noreply.github.com>
2020-06-29 18:25:24 +0300

    Updates

---
+++ precache-manifest.9c67280f13f4dd137f537d0ddb6b6f6b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38732adbaa0ad8f70c44d537397d38f3",
+    "url": "/dart-service-layout/frontend/index.html"
+  },
+  {
+    "revision": "ae085f21c5713a2ca81d",
+    "url": "/dart-service-layout/frontend/static/css/2.a33d7f97.chunk.css"
+  },
+  {
+    "revision": "dfa80c7d279796f7385a",
+    "url": "/dart-service-layout/frontend/static/css/main.d580718f.chunk.css"
+  },
+  {
+    "revision": "ae085f21c5713a2ca81d",
+    "url": "/dart-service-layout/frontend/static/js/2.a3d581ee.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dart-service-layout/frontend/static/js/2.a3d581ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa80c7d279796f7385a",
+    "url": "/dart-service-layout/frontend/static/js/main.6875fae1.chunk.js"
+  },
+  {
+    "revision": "1b901b1c13b2bbc3d05e",
+    "url": "/dart-service-layout/frontend/static/js/runtime-main.b86c31c7.js"
+  },
+  {
+    "revision": "9db900cb8be2db720723de02e33ebe2c",
+    "url": "/dart-service-layout/frontend/static/media/bg1.9db900cb.png"
+  },
+  {
+    "revision": "32142661ac5221ff140e426be47ee727",
+    "url": "/dart-service-layout/frontend/static/media/poster.32142661.png"
+  },
+  {
+    "revision": "f376af1c51fd62e479c27696539c9d86",
+    "url": "/dart-service-layout/frontend/static/media/video.f376af1c.mp4"
+  }
+]);
